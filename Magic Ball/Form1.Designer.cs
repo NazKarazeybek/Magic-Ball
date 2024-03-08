067eb66a79730431d5c6cccb9eac8778a77f9499
NazKarazeybek <elifkara739@ed.amdsb.ca>
2024-03-08 14:13:01 -0500

    Proje dosyası ekle.

---
+++ Magic Ball/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Magic_Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.ballButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(0, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(567, 75);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Think Of A Question";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(-3, 482);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(570, 49);
+            this.outputLabel.TabIndex = 1;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ballButton
+            // 
+            this.ballButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ballButton.BackgroundImage")));
+            this.ballButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ballButton.FlatAppearance.BorderSize = 0;
+            this.ballButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.ballButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.ballButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ballButton.Location = new System.Drawing.Point(-2, 104);
+            this.ballButton.Name = "ballButton";
+            this.ballButton.Size = new System.Drawing.Size(566, 348);
+            this.ballButton.TabIndex = 3;
+            this.ballButton.UseVisualStyleBackColor = true;
+            this.ballButton.Click += new System.EventHandler(this.ballButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(563, 556);
+            this.Controls.Add(this.ballButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Magic Ball";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button ballButton;
+    }
+}
+
---
+++ Magic Ball/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic_Ball
+{
+    public partial class Form1 : Form
+    {
+        Random randomGen = new Random();
+        int ballSwitch = 6;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ballButton_Click(object sender, EventArgs e)
+        {
+            ballSwitch = randomGen.Next(0, 7);
+
+            switch (ballSwitch)
+            {
+                case 0:
+                    outputLabel.Text = "Most likely";
+                    break;
+                case 1:
+                    outputLabel.Text = "Very doubtful";
+                    break;
+                case 2:
+                    outputLabel.Text = "Outlook not so good";
+                    break;
+                case 3:
+                    outputLabel.Text = "Don't count on it";
+                    break;
+                case 4:
+                    outputLabel.Text = "It is certain";
+                    break;
+                case 5:
+                    outputLabel.Text = "Yes";
+                    break;
+                case 6:
+                    outputLabel.Text = "No";
+                    break;
+
+            }
+
+
+
+        }
+    }
+}
